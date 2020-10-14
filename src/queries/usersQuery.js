4f61532ff08d0ac0dfd03ea195cd3291b9e84685
Emmanuel Neni <emmanuelneni@gmail.com>
2020-10-14 16:39:38 +0300

    using constants

--- src/queries/usersQuery.js
+++ src/queries/usersQuery.js
@@ -1,24 +1,26 @@
 const db = require('../../config/dbConfig.js');
 
+const tableNames = require('../constants/tableName');
+
 module.exports = {
   getAll() {
-    return db('users');
+    return db(tableNames.user);
   },
 
   getOne(id) {
-    return db('users').where('id', id).first();
+    return db(tableNames.user).where('id', id).first();
   },
 
   async signUp(users) {
-    const idArray = await db('users').insert(users, 'id');
+    const idArray = await db(tableNames.user).insert(users, 'uuid');
     return idArray[0];
   },
 
   update(id, users) {
-    return db('users').where('id', id).update(users);
+    return db(tableNames.user).where('id', id).update(users);
   },
 
   delete(id) {
-    return db('users').where('id', id).delete();
+    return db(tableNames.user).where('id', id).delete();
   },
 };
